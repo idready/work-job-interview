c1807307f923ecd970b07678a86b26ec9b82a343
idready <you_sombo@hotmail.fr>
2025-10-30 19:24:01 +0100

    Clean up

--- src/app/word-suggestions/word-suggestions-module.ts
+++ src/app/word-suggestions/word-suggestions-module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { WordSuggestions } from './word-suggestions';
-
-
 
 @NgModule({
   declarations: [],
