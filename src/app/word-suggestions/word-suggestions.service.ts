95411c08ae0bdefa40620d6e8acda74b3c19eab5
idready <you_sombo@hotmail.fr>
2025-10-30 19:12:56 +0100

    Add forgotten sorting

--- src/app/word-suggestions/word-suggestions.service.ts
+++ src/app/word-suggestions/word-suggestions.service.ts
@@ -7,7 +7,7 @@ import { IamTheTestInterface, WordCandidate } from './iam-test.interface';
 export class WordSuggestionsService implements IamTheTestInterface {
 
   private getDifferenceScore(word1: string, word2: string): number {
-    if (word1.length !== word2.length) return Number.POSITIVE_INFINITY; // Bring it down if lengths differ
+    if (word1.length !== word2.length) return Number.MAX_SAFE_INTEGER; // Bring it down if lengths differ
     let diff = 0;
     for (let i = 0; i < word1.length; i++) {
       if (word1[i] !== word2[i]) diff++;
@@ -30,6 +30,13 @@ export class WordSuggestionsService implements IamTheTestInterface {
         candidates.push({ diff, lenDiff: Math.abs(term.length - lower.length), value: lower });
       }
     }
+
+    // Sorting
+    candidates.sort((a, b) => {
+      if (a.diff !== b.diff) return a.diff - b.diff;
+      if (a.lenDiff !== b.lenDiff) return a.lenDiff - b.lenDiff;
+      return a.value.localeCompare(b.value);
+    });
 
     return candidates.slice(0, numberOfSuggestions);
   }
